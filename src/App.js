e79c3e08d6909c3645fca7919638165af5315244
Andrzej Rokosz <andrzej_ro@wp.pl>
2018-12-04 12:00:03 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
-
 import TodoList from './TodoList'
-
 
 
 class App extends Component {
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { addTodo } from './store/todos'
+import { addTodo, deleteTodo } from './store/todos'
 import { toggleTodo } from './store/todos'
+
+
 const mapStateToProps = (store) => ({
     _todos: store.todos.allTodos,
     visibleTodos: store.todos.visibleTodos
@@ -10,7 +12,8 @@ const mapStateToProps = (store) => ({
 
 const mapDispatchToProps = (dispatch) => ({
     _addTodo: text => dispatch(addTodo(text)),
-    _toggleTodo: index => dispatch(toggleTodo(index))
+    _toggleTodo: index => dispatch(toggleTodo(index)),
+    _deleteTodo: index => dispatch(deleteTodo(index))
 })
 
 class TodoList extends React.Component {
@@ -31,26 +34,44 @@ class TodoList extends React.Component {
     handleTodoClick = (index) => {
         this.props._toggleTodo(index)
     }
+    handleDelete = (index) => {
+        this.props._deleteTodo(index)
+    }
 
 
     render() {
         console.log(this.props)
         return <div>
+            {this.renderInput()}
+            {this.renderList()}
+        </div>
+    }
+
+    renderInput() {
+        return <div>
             <input onChange={this.handleInputChange} />
             <button onClick={this.handleButtonClick}>Add todo</button>
-
+        </div>
+    }
+    
+    renderList() {
+        return <div>
             {this.props._todos.map((todo, index) =>
-                <div key={todo.text}
+                <div
+                    key={todo.text}
                     style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
                     onClick={() => this.handleTodoClick(index)}>
                     {todo.text}
+                    <button
+                        onClick={() => this.handleDelete(index)}
+                    >
+                        X
+                    </button>
                 </div>
             )}
-
         </div>
-
-
     }
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
